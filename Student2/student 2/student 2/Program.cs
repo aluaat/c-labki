22080ca8b3f78a7fd901e6306f8a749f6a60f501
Alua <aluaat@mail.ru>
2018-02-06 01:03:33 +0600

    Student class-2solution

---
+++ Student2/student 2/student 2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace student_2 
+{
+    class Stud
+    {
+        public string name;
+        public int ID;
+        public float gpa;
+        public Stud()
+        {
+            this.name = Console.ReadLine();
+            this.ID = int.Parse(Console.ReadLine());
+            this.gpa = float.Parse(Console.ReadLine()); //4,5-vvodim s zapyatoi
+            
+        }
+        public override string ToString()
+        {
+            return name+ "^_^"+ID+ "^_^" +gpa;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Student of KBTU:");
+            Stud a = new Stud();
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
