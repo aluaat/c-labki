65f13dbf864d6ec913b2e35e636004877d19ac51
Alua <aluaat@mail.ru>
2018-02-08 00:11:21 +0600

    pravil'noe rewenie 2-solution

---
+++ primenum2/primenum2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primenum2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string alua = Console.ReadLine(); //2 3 4 5 6 7
+            string[] massiv = alua.Split(' '); //sdelala massiv,razdelila probelom
+            bool Prime = true;
+            for (int i = 0; i < massiv.Length; i++) //probegaemsya do konca
+            {
+                
+                if (int.Parse(massiv[i])<=1) //chtob minusov ne schital i do edinichki
+                {
+                    Prime = false;
+                    
+                   
+                }
+
+
+                for (int j = 2; j < int.Parse(massiv[i]); j++)
+                {
+                    if (int.Parse(massiv[i]) % j == 0)
+                    {
+                        Prime = false;
+                        break;
+
+                    }
+
+                }
+                if (Prime == true)
+                {
+                    Console.WriteLine(massiv[i]);
+
+                }
+
+                Prime = true; //obnovlyaem bool ,esli ne pomenyat' bool ostanetsya false-m dlya sleduwix 
+            }
+
+            Console.ReadKey();
+          
+        }
+    }
+}
