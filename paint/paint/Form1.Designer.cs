8a33ccdcd84f6ca814ca504cc27e176753aeac93
Alua <aluaat@mail.ru>
2018-04-27 11:22:51 +0600

    paint

---
+++ paint/paint/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace paint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button5 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 77);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "o";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Violet;
+            this.button2.Location = new System.Drawing.Point(794, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(170, 45);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "CHOOSE COLOR ";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.chooseCOLOR_click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(40, 122);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1129, 387);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(94, 30);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(66, 77);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "rectangle";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.Color.Fuchsia;
+            this.trackBar1.Location = new System.Drawing.Point(599, 12);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(189, 56);
+            this.trackBar1.TabIndex = 12;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button5.Location = new System.Drawing.Point(269, 4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 47);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "FILL";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(970, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 39);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "open";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1063, 3);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 39);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "save";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.button6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button6.Location = new System.Drawing.Point(359, 4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 47);
+            this.button6.TabIndex = 16;
+            this.button6.Text = "ERASER";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Purple;
+            this.button8.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button8.Location = new System.Drawing.Point(450, 3);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 47);
+            this.button8.TabIndex = 17;
+            this.button8.Text = "PEN";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click_1);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1201, 521);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ paint/paint/Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+
+namespace paint
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        // создаем пустую очередь типа string  queue<string> myQueue;
+        //Инициализирует новый экземпляр класса Queue<T>, который является пустым и имеет начальную емкость по умолчанию. 
+        //Queue<T>()      Queue<T>(Int32)
+        Bitmap btm;
+        Queue<Point> q = new Queue<Point>();
+        Pen pen;
+        Point  prev,cur;
+        bool clicked;
+        Color color, fillcolor;
+        int trackbar;
+        public SolidBrush brush;
+        public enum Tool
+        {
+            PEN,
+            CIRCLE ,
+            RECTANGLE ,
+            FILL,
+            ERASER
+        };
+        Tool t = Tool.PEN;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+             btm = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = btm;
+            
+            g = Graphics.FromImage(btm);// sozdaet novy ob'ekt GRAPHICS iz ukazannogo  ob'ekta image=btm
+            g.Clear(Color.White);
+          //  pen = new Pen(Color.Violet, 5);
+            clicked = false;
+           // t = Tool.PEN;
+
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+           
+            
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+       
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            clicked = true;
+            prev = e.Location;
+          
+            
+            
+            if (t == Tool.FILL)
+            {
+                color = btm.GetPixel(e.X, e.Y);
+                q.Enqueue(new Point(e.X, e.Y));
+                btm.SetPixel(e.X, e.Y, fillcolor);
+
+                while (q.Count > 0)
+                {
+                    Point cur = q.Dequeue();
+
+                    FillCheck(cur.X + 1, cur.Y);
+                    FillCheck(cur.X - 1, cur.Y);
+                    FillCheck(cur.X, cur.Y + 1);
+                    FillCheck(cur.X, cur.Y - 1);
+                }
+                pictureBox1.Refresh();
+
+            }
+            
+        }
+       
+        public void FillCheck(int x, int y)
+        {
+            if (x < 0 || y < 0 || x >= pictureBox1.Width || y >= pictureBox1.Height)
+                return;
+            if (btm.GetPixel(x, y) == color)
+            {
+                fillcolor = pictureBox1.BackColor;
+
+                btm.SetPixel(x, y, fillcolor);
+                q.Enqueue(new Point(x, y));
+
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            cur = e.Location; //возвращает расположение указателя мыши в момент создания события мыши
+            if (clicked && t==Tool.PEN ) //clicked && t -чтобы только после клика рисовался 
+            {
+                g.DrawLine(new Pen(pictureBox1.BackColor, trackbar), prev.X, prev.Y, cur.X, cur.Y);
+                 pictureBox1.Refresh();
+                
+                prev = cur;
+            }
+           /* if (clicked && t==Tool.FILL)
+            {
+                g.DrawLine(new Pen(pictureBox1.BackColor, trackbar), prev.X, prev.Y, cur.X, cur.Y);
+                pictureBox1.Refresh();
+                prev = cur;
+            }*/
+            if(clicked==true && t==Tool.ERASER)
+            {
+                g.DrawLine(new Pen(Color.White, trackBar1.Value), prev, cur);
+            }
+            pictureBox1.Refresh();
+        }
+
+        public void Check(int x, int y)
+        {
+            if (x < 0 || y < 0 || x >= pictureBox1.Width || y >= pictureBox1.Height)
+                return;
+
+            if (btm.GetPixel(x, y) == color)
+            {
+                btm.SetPixel(x, y, fillcolor);
+                q.Enqueue(new Point(x, y));
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (clicked && t==Tool.RECTANGLE)
+            {
+                int x = Math.Min(prev.X, cur.X);
+                int y = Math.Min(prev.Y, cur.Y);
+                int w = Math.Abs(prev.X - cur.X);
+                int h = Math.Abs(prev.Y - cur.Y);
+             g.DrawRectangle(new Pen(pictureBox1.BackColor, trackbar), new Rectangle(x, y, w, h));
+                pictureBox1.Refresh();
+                
+
+
+
+                
+            }
+            if (clicked && t==Tool.CIRCLE)
+            {
+                int x = Math.Min(prev.X, cur.X);
+                int y = Math.Min(prev.Y, cur.Y);
+                int w = Math.Abs(prev.X - cur.X);
+                int h = Math.Abs(prev.Y - cur.Y);
+                g.DrawEllipse(new Pen(pictureBox1.BackColor, trackbar), new Rectangle(x, y, w, h));
+                pictureBox1.Refresh();
+            }
+            clicked = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            t = Tool.CIRCLE;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            t = Tool.FILL;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+
+            if (clicked && t == Tool.RECTANGLE)
+            {
+                int x = Math.Min(prev.X, cur.X);
+                int y = Math.Min(prev.Y, cur.Y);
+                int w = Math.Abs(prev.X - cur.X);
+                int h = Math.Abs(prev.Y - cur.Y);
+                e.Graphics.DrawRectangle(new Pen(pictureBox1.BackColor, trackbar), new Rectangle(x, y, w, h));
+               
+
+
+
+
+            }
+            if (clicked && t == Tool.CIRCLE)
+            {
+                int x = Math.Min(prev.X, cur.X);
+                int y = Math.Min(prev.Y, cur.Y);
+                int w = Math.Abs(prev.X - cur.X);
+                int h = Math.Abs(prev.Y - cur.Y);
+                e.Graphics.DrawEllipse(new Pen(pictureBox1.BackColor, trackbar), new Rectangle(x, y, w, h));
+                
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            trackbar = trackBar1.Value;//возвращает численное значение ползука на полосе 
+        }
+
+       
+
+       
+
+
+        private void chooseCOLOR_click(object sender, EventArgs e)
+        {
+             DialogResult colorResult = colorDialog1.ShowDialog();
+                 if (colorResult==DialogResult.OK)
+             {
+                 pictureBox1.BackColor
+                     = colorDialog1.Color; 
+             }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            openFileDialog1  = new OpenFileDialog();
+            openFileDialog1.Filter = "Image Files(.BMP)|.BMP|Image Files(.JPG)|.JPG|Image Files(.GIF)|.GIF";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                openFileDialog1.OpenFile();
+                btm = new Bitmap(openFileDialog1.FileName);
+                pictureBox1.Image = btm;
+                g = Graphics.FromImage(btm);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Image Files(.BMP)|.BMP|Image Files(.JPG)|.JPG|Image Files(.GIF)|.GIF";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                pictureBox1.Image.Save(saveFileDialog1.FileName);
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            t = Tool.ERASER;
+        }
+
+        private void button8_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            t = Tool.RECTANGLE;
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            
+           
+        }
+
+        
+    }
+}
